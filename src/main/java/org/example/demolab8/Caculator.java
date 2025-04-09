21afe8489a483aeb93dd5f9b13ee499269fab889
Son <ronaldo9646@gmail.com>
2025-04-09 17:02:37 +0700

    testfail

--- src/main/java/org/example/demolab8/Caculator.java
+++ src/main/java/org/example/demolab8/Caculator.java
@@ -3,7 +3,7 @@ package org.example.demolab8;
 public class Caculator implements Icaculator{
 
     public Integer sum(Integer a, Integer b){
-        return 0;
+        return a+b;
     }
 
     public Integer sub(Integer a, Integer b){
