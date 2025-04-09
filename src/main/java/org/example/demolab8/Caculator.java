9e76f652802bc093ed177d86f614102c4abd1fa1
Son <ronaldo9646@gmail.com>
2025-04-09 17:16:11 +0700

    pass

--- src/main/java/org/example/demolab8/Caculator.java
+++ src/main/java/org/example/demolab8/Caculator.java
@@ -3,10 +3,10 @@ package org.example.demolab8;
 public class Caculator implements Icaculator{
 
     public Integer sum(Integer a, Integer b){
-        return 0;
+        return a+b;
     }
 
     public Integer sub(Integer a, Integer b){
-        return 0;
+        return a-b;
     }
 }
